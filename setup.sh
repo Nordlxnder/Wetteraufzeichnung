dff8fb9830764f37c5b8d472acc3933dba3cc795
Nordlxnder <lnxawdr@web.de>
2017-10-29 09:54:41 +0100

    setup hinzugefügt

--- setup.sh
+++ setup.sh
@@ -14,5 +14,5 @@ sudo cp wetterdaten_speichern.py /mnt/skripte/wetterdaten_speichern.py
 sudo cp ./Beispiel_systemd/wetterdatenspeicherungs* /usr/bin/
 sudo cp ./Beispiel_systemd/wetterdatenspeicherung.service /etc/systemd/system/
 
-sudo systemctl start wetterstation.service
-sudo systemctl enable wetterstation.service
+sudo systemctl start wetterdatenspeicherung.service
+sudo systemctl enable wetterdatenspeicherung.service
